e12a5ac84c905ef8d61c19f43d7ba81b68bdf6ba
tshanno <tom_shannon@rush.edu>
2019-07-02 06:01:48 -0500

    initial commit

---
+++ notes/auto/Notes on what helps as a course director.el
@@ -0,0 +1,39 @@
+(TeX-add-style-hook
+ "Notes on what helps as a course director"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("hyperref" "colorlinks=true" "linkcolor=blue" "urlcolor=blue" "citecolor=blue" "anchorcolor=blue")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "booktabs"
+    "hyperref")
+   (TeX-add-symbols
+    '("specialcell" ["argument"] 2)
+    '("trsem" 1)))
+ :latex)
+
